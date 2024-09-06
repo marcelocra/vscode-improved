994f76022399f235d30bd35b57e7392e415c722d
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-09-05 21:24:12 -0300

    feat: change function name

--- src/extension.ts
+++ src/extension.ts
@@ -15,11 +15,11 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   const disposable = vscode.commands.registerCommand(
-    "vscode-improved.helloWorld",
+    "vscode-improved.showDateTimeNow",
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from vscode-improved!");
+      vscode.window.showInformationMessage(new Date().toLocaleString());
     }
   );
 
